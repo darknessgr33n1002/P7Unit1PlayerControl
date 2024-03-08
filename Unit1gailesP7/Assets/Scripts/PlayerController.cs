4d3d8177a37e03e9ad711682629bd719624ee0d2
darknessgr33n1002 <145072424+darknessgr33n1002@users.noreply.github.com>
2024-03-08 15:28:47 -0800

    me car can offifclty go grrrr now lol

--- Unit1gailesP7/Assets/Scripts/PlayerController.cs
+++ Unit1gailesP7/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5.0f;
+    public float turnSpeed;
+    public float horizontalInput;
+    public float forwardInput;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +19,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        transform.Translate(Vector3.forward * Time.deltaTime * 15);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
